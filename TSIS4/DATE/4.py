32c7601a05ffd6289d09d33cc138ab96d8ee324a
NuraKunanbaev <dauletkaliev060@gmail.com>
2023-02-23 23:02:49 +0600

    1

--- TSIS4/DATE/4.py
+++ TSIS4/DATE/4.py
@@ -1,6 +1,6 @@
 import datetime
 dt1 = datetime.datetime.today().replace(microsecond = 0)
-dt2 = datetime.datetime(2006, 2, 21, hour=5, minute=38, second=25, microsecond=0, tzinfo=None)
+dt2 = datetime.datetime(2004, 12, 6, hour=5, minute=38, second=25, microsecond=0, tzinfo=None)
 def difference(dt1, dt2):
     dif = dt1 - dt2
     return dif.days*24*3600 + dif.seconds
