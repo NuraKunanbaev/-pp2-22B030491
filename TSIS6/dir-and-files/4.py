3abeb94e81382740efb563012c3c20eae1fc5485
NuraKunanbaev <dauletkaliev060@gmail.com>
2023-03-07 13:12:04 +0600

    "

--- TSIS6/dir-and-files/4.py
+++
@@ -1,4 +0,0 @@
-import os
-f = open('3.py', 'r')
-l = len(f.readlines())
-print(l)
--- TSIS6/dir-and-files/7.py
+++
@@ -1,6 +0,0 @@
-# Write a Python program to copy the
-# contents of a file to another file
-
-with open("A.txt", 'r') as firstfile, open('B.txt', 'a') as secondfile:
-    for line in firstfile:
-        secondfile.write(line)
